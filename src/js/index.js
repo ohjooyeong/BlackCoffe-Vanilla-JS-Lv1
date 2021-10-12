10e8266282630c2e85f7bb5a9d1c0ae3ef59d446
OH-JOO-YEONG <brb1111@naver.com>
2021-10-12 23:09:31 +0900

    feat: 메뉴 수정하기 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 // step1 요구사항 구현을 위한 전략
 // TO-DO 메뉴 추가
 // - [x] 메뉴의 이름을 입력받고 엔터키 입력으로 추가한다.
-// - [ ] 메뉴의 이름을 입력받고 확인 버튼을 클릭하면 메뉴를 추가한다.
+// - [x] 메뉴의 이름을 입력받고 확인 버튼을 클릭하면 메뉴를 추가한다.
 // - [x] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
 // - [x] 총 메뉴 갯수를 count하며 상단에 보여준다.
 // - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
@@ -10,6 +10,22 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
+  // TO-DO 메뉴 수정
+  // - [x] 메뉴의 수정 버튼클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
+  // - [x] 메뉴창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+
+  // 이벤트 위임 최상위 노드를 이벤트를 지정해서 하위 노드의 이벤트들을 각각 접근할 수 있다.
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    if (e.target.classList.contains("menu-edit-button")) {
+      const $menuName = e.target.closest("li").querySelector(".menu-name");
+      const updatedMenuName = prompt(
+        "메뉴명을 수정하세요",
+        $menuName.innerText
+      );
+      $menuName.innerText = updatedMenuName;
+    }
+  });
+
   // form 태그가 자동으로 전송되는 걸 막아준다.
   $("#espresso-menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
@@ -66,10 +82,6 @@ function App() {
 }
 
 App();
-
-// TO-DO 메뉴 수정
-// - [ ] 메뉴의 수정 버튼클릭 이벤트를 받고, 메뉴 수정하는 모달창이 뜬다.
-// - [ ] 메뉴창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
 
 // TO-DO 메뉴 삭제
 // - [ ] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
