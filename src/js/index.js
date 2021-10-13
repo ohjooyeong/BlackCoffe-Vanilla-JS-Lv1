92fda3f67d5105cf7b03c9137ba2b3c2f4a0bdf8
OH-JOO-YEONG <brb1111@naver.com>
2021-10-13 22:26:24 +0900

    feat: 메뉴 삭제하기 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,23 @@
 // - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
+// TO-DO 메뉴 수정
+// - [x] 메뉴의 수정 버튼클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
+// - [x] 메뉴창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+
+// TO-DO 메뉴 삭제
+// - [x] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
+// - [x] 확인 버튼을 클릭하면 메뉴가 삭제된다.
+// - [x] 총 메뉴 갯수를 count하며 상단에 보여준다.
+
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-  // TO-DO 메뉴 수정
-  // - [x] 메뉴의 수정 버튼클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
-  // - [x] 메뉴창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+  const updateMenuCount = () => {
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+
+    $(".menu-count").innerText = `총 ${menuCount} 개`;
+  };
 
   // 이벤트 위임 최상위 노드를 이벤트를 지정해서 하위 노드의 이벤트들을 각각 접근할 수 있다.
   $("#espresso-menu-list").addEventListener("click", (e) => {
@@ -23,6 +34,13 @@ function App() {
         $menuName.innerText
       );
       $menuName.innerText = updatedMenuName;
+    }
+
+    if (e.target.classList.contains("menu-remove-button")) {
+      if (confirm("정말 삭제하시겠습니까?")) {
+        e.target.closest("li").remove();
+        updateMenuCount();
+      }
     }
   });
 
@@ -61,10 +79,7 @@ function App() {
       "beforeend",
       menuItemTemplate(espressoMenuName)
     );
-
-    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-
-    $(".menu-count").innerText = `총 ${menuCount} 개`;
+    updateMenuCount();
     $("#espresso-menu-name").value = "";
   };
 
@@ -82,8 +97,3 @@ function App() {
 }
 
 App();
-
-// TO-DO 메뉴 삭제
-// - [ ] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
-// - [ ] 확인 버튼을 클릭하면 메뉴가 삭제된다.
-// - [ ] 총 메뉴 갯수를 count하며 상단에 보여준다.
