0d3e62ccca66880e6400bb9f433341cd57120ea4
OH-JOO-YEONG <brb1111@naver.com>
2021-10-16 15:06:34 +0900

    fix: 수정 기능 변수 잘못 기재로 인한 기능 오작동 수정
    - this.menu[menuId].name 에다 updateMenuName 함수를 넣으면서 값이 함수로 넣어진 오류 발생
    - updateMenuName -> updatedMenuName 값 제대로 기입

--- src/js/index.js
+++ src/js/index.js
@@ -84,7 +84,7 @@ function App() {
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
 
-    this.menu[menuId].name = updateMenuName;
+    this.menu[menuId].name = updatedMenuName;
     store.setLocalStorage(this.menu);
     $menuName.innerText = updatedMenuName;
   };
